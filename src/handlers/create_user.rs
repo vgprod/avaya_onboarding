9b7543925d9e939e3e9419b31c23217efee47292
Vladan <gezuumma@protonmail.com>
2024-03-24 04:10:42 +0100

    Accepting user data from Json in format: {  first_name: ADA,  last_name: ADASD,  age: 30,  diplomas: [dipl]}

--- src/handlers/create_user.rs
+++ src/handlers/create_user.rs
@@ -1,21 +1,18 @@
 use crate::{models::Employee, storage::save_employee_to_file};
-use uuid::Uuid;
+use axum::{extract, http::StatusCode, response};
 use chrono::Utc;
-use axum::{
-    http::StatusCode,
-    response,
-    extract
-};
+use uuid::Uuid;
 
-pub async fn create_user(input:extract::Json<Employee>) -> Result<response::Json<Employee>, StatusCode> {
-
+pub async fn create_user(
+    input: extract::Json<Employee>,
+) -> Result<response::Json<Employee>, StatusCode> {
     let first_name = input.first_name.clone();
-    let last_name =   input.last_name.clone();
+    let last_name = input.last_name.clone();
     let user_handle = generate_handle(&first_name, &last_name).await;
     let password = generate_password().await;
 
     let new_employee = Employee::new(
-        Some(Uuid::new_v4().to_string()), 
+        Some(Uuid::new_v4().to_string()),
         first_name,
         last_name,
         Some(user_handle),
@@ -25,17 +22,15 @@ pub async fn create_user(input:extract::Json<Employee>) -> Result<response::Json
         Some(Utc::now()),
     );
     save_employee_to_file(&new_employee).unwrap();
-    
 
-   Ok(response::Json(new_employee))
+    Ok(response::Json(new_employee))
 }
 
-
-async fn generate_handle(first:&str, last:&str ) -> String{
+async fn generate_handle(first: &str, last: &str) -> String {
     let handle = format!("{}.{}", first, last);
     handle
 }
 
-async fn generate_password() -> String{
+async fn generate_password() -> String {
     "password".to_string()
 }
