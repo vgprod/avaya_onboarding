bef7d7097ff3d833bec325037a7213532863cb76
Vladan <gezuumma@protonmail.com>
2024-03-24 04:08:22 +0100

    Accepting user data from Json in format:

--- src/handlers/create_user.rs
+++ src/handlers/create_user.rs
@@ -3,25 +3,39 @@ use uuid::Uuid;
 use chrono::Utc;
 use axum::{
     http::StatusCode,
-    response::Json,
+    response,
+    extract
 };
 
-pub async fn create_user() -> Result<Json<Employee>, StatusCode> {
+pub async fn create_user(input:extract::Json<Employee>) -> Result<response::Json<Employee>, StatusCode> {
+
+    let first_name = input.first_name.clone();
+    let last_name =   input.last_name.clone();
+    let user_handle = generate_handle(&first_name, &last_name).await;
+    let password = generate_password().await;
+
     let new_employee = Employee::new(
-        Uuid::new_v4().to_string(), 
-        "John".to_string(),
-        "Doe".to_string(),
-        "userhandle".to_string(),
-        "pass".to_string(),
-        30,
-        vec![],
-        Utc::now(),
+        Some(Uuid::new_v4().to_string()), 
+        first_name,
+        last_name,
+        Some(user_handle),
+        Some(password),
+        input.age.clone(),
+        input.diplomas.clone(),
+        Some(Utc::now()),
     );
     save_employee_to_file(&new_employee).unwrap();
     
 
-   Ok(Json(new_employee))
+   Ok(response::Json(new_employee))
 }
 
 
+async fn generate_handle(first:&str, last:&str ) -> String{
+    let handle = format!("{}.{}", first, last);
+    handle
+}
 
+async fn generate_password() -> String{
+    "password".to_string()
+}
--- src/models/employee.rs
+++ src/models/employee.rs
@@ -4,18 +4,18 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Employee {
-    id: String,
-    first_name: String,
-    last_name: String,
-    user_handle: String,
-    password: String,
-    age: u8,
-    diplomas: Vec<String>,
-    created_at: chrono::DateTime<Utc>,
+    id: Option<String>,
+    pub first_name: String,
+    pub last_name: String,
+    user_handle: Option<String>,
+    password: Option<String>,
+    pub age: u8,
+    pub diplomas: Vec<String>,
+    created_at: Option<chrono::DateTime<Utc>>,
 }
 
 impl Employee {
-    pub fn new(id: String, first_name: String, last_name: String, user_handle: String, password: String, age: u8, diplomas: Vec<String>, created_at: chrono::DateTime<Utc>) -> Self {
+    pub fn new(id: Option<String>, first_name: String, last_name: String, user_handle: Option<String>, password: Option<String>, age: u8, diplomas: Vec<String>, created_at: Option<chrono::DateTime<Utc>>) -> Self {
         Self { id, first_name, last_name, user_handle, password, age, diplomas, created_at }
     }
 }
