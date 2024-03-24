c46b03829528326d3b5c35af0755814e17f39c64
Vladan <gezuumma@protonmail.com>
2024-03-24 23:23:32 +0100

    Changed route /applicants to display only ID's

--- src/handlers/get_applicants.rs
+++ src/handlers/get_applicants.rs
@@ -4,15 +4,16 @@ use axum::response::Json;
 //Retrieves a collection of all non boraded applicants in JSON format.
 //ex. http://127.0.0.1:8100/applicants
 
-pub async fn get_applicants() -> Json<Vec<Employee>> {
+pub async fn get_applicants() -> Json<Vec<String>> {
     let employees = load_employees_from_file().await;
     match employees {
         Ok(users) => {
-            let non_onboarded_employees: Vec<Employee> = users
+            let non_onboarded_employees  = users
                 .iter()
                 .filter(|employee| !employee.onboarded.expect("Error"))
-                .cloned()
+                .map(|non_onb| non_onb.id.clone().expect("Error"))
                 .collect();
+            //let vecid = non_onboarded_employees.iter().map(|non_onb| non_onb.id.clone().expect("Error")).collect();
             Json(non_onboarded_employees)
         }
         Err(err) => {
